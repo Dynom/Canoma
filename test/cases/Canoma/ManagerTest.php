b9e16c13f39f0d5901ad5359bc36c6778606db76
Mark van der Velden <mark@dynom.nl>
2012-05-27 20:51:18 +0200

    Wrote the test that adding a node would yield 1 node to be added.

--- test/cases/Canoma/ManagerTest.php
+++ test/cases/Canoma/ManagerTest.php
@@ -14,4 +14,18 @@ class ManagerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('\Canoma\Manager', $manager);
     }
+
+
+    public function testAddNode()
+    {
+        $manager = new \Canoma\Manager(
+            new \Canoma\HashAdapter\Md5(),
+            42
+        );
+
+        $this->assertEquals(0, count($manager->getAllNodes()), 'Expecting exactly 0 nodes since none were added.');
+
+        $manager->addNode('foo');
+        $this->assertEquals(1, count($manager->getAllNodes()), 'Expecting exactly 1 node after adding 1');
+    }
 }

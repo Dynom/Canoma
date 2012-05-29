9d931cf9fcb95a7a7209fa95dbe89dc765ebb115
Mark van der Velden <mark@dynom.nl>
2012-05-29 22:22:31 +0200

    Added some documentation

--- test/calculateReplicates.php
+++ test/calculateReplicates.php
@@ -1,5 +1,25 @@
 <?php
-
+/**
+ * The standard deviation test script.
+ *
+ * Call like:
+ *
+ *     php calculateReplicates.php [number of replicates]
+ *
+ *
+ * This script is used to test about what settings you want to use in your project. There are a couple of parameters you
+ * might want to fiddle with:
+ *
+ * - The hashing adapter
+ * - The amount of replicates (per node)
+ * - The amount of nodes
+ * - The size of your cache and node keys (string-length of the argument for 'addNode' and 'getNodeForString')
+ * - The amount of cache-keys
+ *
+ * Want to learn more? Be sure to read the documentation on: https://github.com/Dynom/Canoma
+ *
+ * Have fun!
+ */
 require __DIR__ .'/bootstrap.php';
 
 // The number and the algorithm you want to play with
@@ -9,7 +29,7 @@ $nodes = 2;
 
 // The amount of cache-keys we'll be storing to simulate. A higher value means more accuracy and more real-world,
 // but it also takes longer to complete.
-$cacheKeys = 100;
+$cacheKeys = 10000;
 
 // ---------------------------------------------------------------------------------------------------------------------
 
@@ -45,6 +65,11 @@ print_r(array_count_values($result));
 printf("Spent %0.3f seconds doings lookups.", $stop - $start);
 
 
+
+/**
+ * @param array $result
+ * @return float
+ */
 function calculateStandardDeviation(array $result)
 {
     $resultSummary = array_count_values($result);
